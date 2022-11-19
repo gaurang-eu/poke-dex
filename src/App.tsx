9f3627bdc0cf0e307b1d3d4326b76a37960f161f
gaurang-eu <gpxaviers@yahoo.co.in>
2022-11-20 02:32:22 +0530

    eslint implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,73 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react'
 
-import { useSelector, useDispatch } from "react-redux";
-import { RootState } from "./pokedex-redux/store";
-import { putPokemonList, queryPokemonStateList } from './pokedex-redux/PokemenReducer';
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from './pokedex-redux/store'
+import { putPokemonList, queryPokemonStateList } from './pokedex-redux/PokemenReducer'
 
-import { useQuery } from '@apollo/client/react/hooks';
+import { useQuery } from '@apollo/client/react/hooks'
 
-import './App.css';
+import './App.css'
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import PokemonCard from './component/PokemonCard';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import PokemonCard from './component/PokemonCard'
 
-import Header from './component/Header';
+import Header from './component/Header'
 
-import { PokemanResult } from './types/PokemonResult';
-import { getMorePokemon } from './graphql/queries';
-import LoadMore from './component/LoadMore';
-import Loader from './component/Loader';
-import AlertMessage from './component/AlertMessage';
-import { INITIAL_OFFSET, LIST_LIMIT } from './util/constants';
+import { PokemanResult } from './types/PokemonResult'
+import { getMorePokemon } from './graphql/queries'
+import LoadMore from './component/LoadMore'
+import Loader from './component/Loader'
+import AlertMessage from './component/AlertMessage'
+import { INITIAL_OFFSET, LIST_LIMIT } from './util/constants'
 
+function App () {
+  const allPokemonsLoaded = useSelector((state: RootState) => state.pokemons.allPokemonsLoaded)
+  const queryPokemons = useSelector((state: RootState) => state.pokemons.queryPokemons)
+  const dispatch = useDispatch()
 
-
-function App() {
-
-  const allPokemonsLoaded = useSelector((state: RootState) => state.pokemons.allPokemonsLoaded);
-  const queryPokemons = useSelector( (state: RootState) => state.pokemons.queryPokemons);
-  const dispatch = useDispatch();
-
-  const [offset, setOffset] = useState<number>(INITIAL_OFFSET);
-  const { loading, error, data } = useQuery<PokemanResult>(getMorePokemon(-1));
-  const [moreLoadingPressed, setMoreLoadingPressed] = useState<boolean>(false);
+  const [offset, setOffset] = useState<number>(INITIAL_OFFSET)
+  const { loading, error, data } = useQuery<PokemanResult>(getMorePokemon(-1))
+  const [moreLoadingPressed, setMoreLoadingPressed] = useState<boolean>(false)
 
   useEffect(() => {
-        dispatch(queryPokemonStateList({offset: offset, limit: LIST_LIMIT }))
-  }, [allPokemonsLoaded, offset]);
+    dispatch(queryPokemonStateList({ offset, limit: LIST_LIMIT }))
+  }, [allPokemonsLoaded, offset])
 
   const loadMorePokemon = useCallback(() => {
-    console.log('loadMorePokemon called');
-    setMoreLoadingPressed(true);
-    setOffset(offset + LIST_LIMIT);
+    console.log('loadMorePokemon called')
+    setMoreLoadingPressed(true)
+    setOffset(offset + LIST_LIMIT)
     // setPokemonListQuery({offset: offset + LIST_LIMIT, limit: LIST_LIMIT })
-  }, [offset]);
+  }, [offset])
 
   // The reference will be used to detect list end
-  const intersecObserver = useRef<IntersectionObserver | null>(null);
+  const intersecObserver = useRef<IntersectionObserver | null>(null)
 
-  const lastPokemonRef = useCallback((pokemon: any) => {
-    if (loading === true) { return }
-    if (intersecObserver.current) { intersecObserver.current.disconnect() }
+  const lastPokemonRef = useCallback((pokemonEle: any) => {
+    if (loading) { return }
+    if (intersecObserver.current != null) { intersecObserver.current.disconnect() }
 
     intersecObserver.current = new IntersectionObserver(pokemons => {
       if (pokemons[0].isIntersecting) {
-        console.log('We are possibaly at the last Pokemon');
-        if(moreLoadingPressed)
-        loadMorePokemon();
+        console.log('We are possibaly at the last Pokemon')
+        if (moreLoadingPressed) { loadMorePokemon() }
       }
     })
 
-    if (pokemon) { intersecObserver.current.observe(pokemon) }
-
+    if (pokemonEle) { intersecObserver.current.observe(pokemonEle) }
   }, [loading, loadMorePokemon])
 
-  let MainLayout = null;
-  let MoreLoadingLayout = null;
+  let MainLayout = null
+  let MoreLoadingLayout = null
 
-
-  if (moreLoadingPressed === false && loading) {
-    MainLayout = <Loader />;
-  } else if (error) {
+  if (!moreLoadingPressed && loading) {
+    MainLayout = <Loader />
+  } else if (error != null) {
     MainLayout = <AlertMessage message={error.message} />
-  } else if (data && data.allPokemon) {
-      dispatch(putPokemonList(data.allPokemon));
-      console.log('queryPokemons.length ' + queryPokemons.length + ' ' + allPokemonsLoaded)
+  } else if ((data != null) && data.allPokemon) {
+    dispatch(putPokemonList(data.allPokemon))
   }
 
   if (queryPokemons && queryPokemons.length > 1) {
@@ -82,19 +75,19 @@ function App() {
     {queryPokemons.map((pokemon, index) => {
       if (queryPokemons.length === index + 1) {
         return (
-          <PokemonCard ref={lastPokemonRef} front_default={pokemon.sprites.front_default} back_default={pokemon.sprites.back_default} name={pokemon.name} id={pokemon.id} types={pokemon.types} />
+          <PokemonCard key={pokemon.id} ref={lastPokemonRef} front_default={pokemon.sprites.front_default} back_default={pokemon.sprites.back_default} name={pokemon.name} id={pokemon.id} types={pokemon.types} />
         )
       }
       return (
-        <PokemonCard front_default={pokemon.sprites.front_default} back_default={pokemon.sprites.back_default} name={pokemon.name} id={pokemon.id} types={pokemon.types} />
+        <PokemonCard key={pokemon.id} front_default={pokemon.sprites.front_default} back_default={pokemon.sprites.back_default} name={pokemon.name} id={pokemon.id} types={pokemon.types} />
       )
     })}</Row>
-  } 
+  }
 
-  if (moreLoadingPressed === false && loading === false) {
-    MoreLoadingLayout = <LoadMore loadMorePokemon={loadMorePokemon} showLoadMoreButton={true} />;
-  } else if (moreLoadingPressed === true && loading === true) {
-    MoreLoadingLayout = <LoadMore loadMorePokemon={loadMorePokemon} showLoadMoreButton={false} />;
+  if (!moreLoadingPressed && !loading) {
+    MoreLoadingLayout = <LoadMore loadMorePokemon={loadMorePokemon} showLoadMoreButton={true} />
+  } else if (moreLoadingPressed && loading) {
+    MoreLoadingLayout = <LoadMore loadMorePokemon={loadMorePokemon} showLoadMoreButton={false} />
   }
 
   return (
@@ -106,7 +99,7 @@ function App() {
       {MainLayout}
       {MoreLoadingLayout}
     </Container>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-import { ApolloProvider } from '@apollo/client/react';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react'
+import { ApolloClient, InMemoryCache } from '@apollo/client'
 
-import { store } from './pokedex-redux/store';
-import { Provider } from 'react-redux';
+import { store } from './pokedex-redux/store'
+import { Provider } from 'react-redux'
 
 const client = new ApolloClient({
   uri: 'https://dex-server.herokuapp.com/graphql/',
   cache: new InMemoryCache()
-});
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -27,9 +27,9 @@ root.render(
     </ApolloProvider>
     </Provider>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
